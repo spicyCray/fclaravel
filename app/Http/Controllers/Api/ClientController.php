13733778922e38f98627691c98000afdec51de80
wugao <1162423676@qq.com>
2020-04-15 22:32:40 +0800

    first commit

---
+++ app/Http/Controllers/Api/ClientController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ClientController extends Controller
+{
+
+
+    public function client()
+    {
+
+    }
+}
